96a8c1b0d0534c22c6332d2aa0c0403fd344c677
AGD3 <49073762+AGD3@users.noreply.github.com>
2020-03-25 16:35:51 -0700

    first commit

---
+++ stringMethods.js
@@ -0,0 +1,74 @@
+// // let coffee = "French";
+// console.log(coffee);
+
+// let flavor = " Vanilla";
+// console.log(flavor);
+
+// let order = coffee + " " + flavor;
+// console.log(order);
+
+// console.log(coffee.concat("",flavor));
+
+// let coffee = 3;
+// let chai = 10;
+// console.log(` I have ${coffee} coffees.`);
+// console.log(`I also have ${chai} chais.`);
+// console.log(`I have a total of ${coffee + chai} drinks total. `);
+
+// function myFunction() {
+
+//     let str = "Can you guess what is the index of 42 is? I am sure you will be surprised!";
+  
+//     let answer = str.indexOf(42);
+  
+//     console.log(answer);
+  
+// }
+
+// function myFunction() {
+
+//     let str = "Can you guess how long this string is? I am sure you will be surprised! (Hint:It is not 42.)";
+  
+//     let answer = str.length;
+  
+//   myFunction(console.log(answer));
+  
+//   }
+
+// function myFunction() {
+
+//     let str1 = "How you doin?";
+  
+//     let str2 = myFunction(str1.pop);
+  
+//   console.log(str2);
+  
+//   }
+
+// function myFunction() {
+
+//     var str = "         SOCIAL DISTANCING!!!                    ";
+  
+//     alert(str.trim);
+  
+//   }
+
+//   function myFunction() {
+
+//     let str = "social distancing!!";
+  
+//     let res = str.toLocaleUpperCase;
+  
+//     console.log(res);
+  
+//   }
+
+function myFunction() {
+
+    let str = "QuiEt VoICeS...PLEASE";
+  
+    let res = str.toLocaleUpperCase;
+  
+    console.log(res);
+  
+  }
